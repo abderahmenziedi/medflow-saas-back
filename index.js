63afd3dd99f44962b5005b83d65bb0de2d1947a2
abdouziadi <abderahmenziedi@gmail.com>
2025-11-20 02:33:41 +0100

    register login works

---
+++ index.js
@@ -0,0 +1,42 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import authRoutes from './Routes/auth.js';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+const corsOptions = {
+  origin: true
+};
+
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+// DB Connection
+mongoose.set('strictQuery', false);
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('MongoDB connected...');
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+  }
+};
+
+// middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors(corsOptions));
+app.use('/api/v1/auth', authRoutes); // ← seule route, en import ESM
+
+app.listen(port, () => {
+  connectDB();
+  console.log('Server started on port ' + port);
+});
